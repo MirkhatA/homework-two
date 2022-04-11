9dd8c512d2e08625e4a1585d489e97d4d8ad2375
Mirkhat Asen <asen.mirkhat@bk.ru>
2022-04-11 15:45:45 +0600

    commit

--- src/com/mirkek/FifthTask.java
+++ src/com/mirkek/FifthTask.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class FifthTask {
     public static void main(String[] args) {
+
+        System.out.print("Enter a string: ");
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
 
+        System.out.print("Enter a symbol: ");
+        char ch = sc.next().charAt(0);
 
         char[] chars = str.toCharArray();
-        symbolFrequency(chars);
+        symbolFrequency(chars, ch);
     }
 
-    public static void symbolFrequency(char[] chars) {
+    public static void symbolFrequency(char[] chars, char ch) {
+        int result = 0;
+
         for (int i = 0; i < chars.length; i++) {
-            int result = 0;
-
-            for (int j = 0; j < chars.length; j++) {
-                if (chars[i] == chars[j]) {
-                    result++;
-                }
-            }
-
-            System.out.println(chars[i] + ": " + result);
+            if (chars[i] == ch) result++;
         }
+
+        System.out.println(result);
     }
 
 }
