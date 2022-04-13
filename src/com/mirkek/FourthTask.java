f556cd6aa665ba7d880f9b10dbb78eebc253bf10
Mirkhat Asen <asen.mirkhat@bk.ru>
2022-04-13 17:23:42 +0600

    commit

--- src/com/mirkek/FourthTask.java
+++ src/com/mirkek/FourthTask.java
@@ -1,13 +1,25 @@
 package com.mirkek;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FourthTask {
     public static void main(String[] args) {
-        String str = "fourth task";
-
-        System.out.println(removeDuplicate(str));
+        String str = "fourth taskf";
+        char[] chars = str.toCharArray();
+        System.out.println(removeDuplicate(chars));
     }
 
-    public static String removeDuplicate(String str) {
-        return "";
+    public static String removeDuplicate(char[] chars) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char ch : chars) {
+            for (int i = 0; i < chars.length; i++) {
+                if (ch == chars[i]) break;
+                else if (i == chars.length - 1) stringBuilder.append(ch);
+            }
+        }
+
+        return stringBuilder.toString();
     }
 }
